6bc15bdf11fe74e45c69a87e9796710b52e7ab7c
yashwantray361 <yashwantray361@gmail.com>
2026-04-07 02:11:53 +0530

    feat: update Vite configuration and add new assets for Avatar Generator
    - Changed Vite plugin for React to use @vitejs/plugin-react-swc for improved performance.
    - Added new avatar images (avt.jpg) and SVG icons (vite.svg, react.svg) to the public directory.
    - Introduced a new data file (data.js) containing various avatar styles and their corresponding API URLs for easy access.

--- Avatar-Generator/src/App.jsx
+++ Avatar-Generator/src/App.jsx
@@ -1,58 +1,145 @@
-import { Scale } from 'lucide-react'
-import { React, useState } from 'react'
-import { data } from './dice/dicebear.js'
+import 'remixicon/fonts/remixicon.css'
+import 'animate.css'
+import { useEffect, useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
 
+const data = [
+  {
+    label: "Illustration",
+    value: "illustration",
+    url: "https://api.dicebear.com/7.x/avataaars/svg?seed="
+  },
+  {
+    label: "Cartoon",
+    value: "cartoon",
+    url: "https://api.dicebear.com/7.x/adventurer/svg?seed="
+  },
+  {
+    label: "Sketchy",
+    value: "sketchy",
+    url: "https://api.dicebear.com/7.x/croodles/svg?seed="
+  },
+  {
+    label: "Robots",
+    value: "robots",
+    url: "https://api.dicebear.com/7.x/bottts/svg?seed="
+  },
+  {
+    label: "Art",
+    value: "art",
+    url: "https://api.dicebear.com/7.x/pixel-art/svg?seed="
+  },
+  {
+    label: "Illustration",
+    value: "illustration",
+    url: "https://api.dicebear.com/7.x/avataaars/svg?seed="
+  },
+  {
+    label: "Male",
+    value: "male",
+    url: "https://randomuser.me/api/portraits/men"
+  },
+  {
+    label: "Female",
+    value: "female",
+    url: "https://randomuser.me/api/portraits/women"
+  }
+]
 
-export default function App() {
+const App = () => {
+  const [src, setSrc] = useState(null)
+  const [option, setOption] = useState("male")
+
+  const generateNumForPerson = ()=>{
+    const r = Math.floor(Math.random() * 99)+1
+    return r
+  }
+
+  const generate = ()=>{
+    const obj = data.find((item)=>item.value === option)
+    const url = obj.url
+    if(option === "male" || option === "female")
+    {
+      const imageUrl = `${url}/${generateNumForPerson()}.jpg`
+      setSrc(imageUrl)
+    }
+    else {
+      const uniqueValue = Date.now()
+      const imageUrl = `${url}${uniqueValue}`
+      setSrc(imageUrl)
+    }
+  }
+
+  const onOptionChange = (e)=>{
+    const value = e.target.value
+    setOption(value)
+  }
+
+  const download = (url)=>{
+    const a = document.createElement("a")
+    a.href = url
+    a.download = `${Date.now()}.jpg`
+    a.click()
+    a.remove()
+  }
+
+  const copy = (url)=>{
+    navigator.clipboard.writeText(url)
+    toast.success("Image url copied", {position: "top-center"})
+  }
+
+  useEffect(()=>{
+    generate()
+  }, [option]) 
+
   return (
-    <>
-      <div
-        className='animate__animated animate__fadeIn overflow-hidden min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 flex items-center justify-center text-white'>
-        <div className="animate__animated animate__slideInUp animate__faster gap-6 flex flex-col items-center w-full max-w-md rounded-2xl shadow-xl backdrop-blur-xl border border-slate-700 p-10">
-          <img
-            className='w-32 h-32 rounded-full border-4 border-slate-700 shadow-lg object-cover'
-            src='https://imgs.search.brave.com/ohAuTp3hK89mgurwQr18x7s-vbzmC8bs0Cu6ZuQZSlQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMjcv/OTUxLzEzNy9zbWFs/bC9zdHlsaXNoLXNw/ZWN0YWNsZXMtZ3V5/LTNkLWF2YXRhci1j/aGFyYWN0ZXItaWxs/dXN0cmF0aW9ucy1w/bmcucG5n'
-         
+    <div className='animate__animated animate__fadeIn overflow-hidden min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 flex items-center justify-center text-white'>
+      <div className='animate__animated animate__slideInUp animate__faster gap-6 flex flex-col items-center w-full max-w-md rounded-2xl shadow-xl backdrop-blur-xl border border-slate-700 p-10'>
+        <div className='bg-white rounded-full'>
+          <img 
+            src={src || "/avt.jpg"}
+            className='w-32 h-32 rounded-full shadow-lg object-cover'  
           />
-          <div className='text-center'>
-            <h1 className='text-3xl font-bold tracking-wide'>Avatar Generator</h1>
-            <p className='text-slate-300'>Generate Unlimated avatars for webistes</p>
-          </div>
+        </div>
+        <div className='text-center'>
+          <h1 className='text-3xl font-bold tracking-wide'>Avatar Generator</h1>
+          <p className='text-slate-300'>Generate unlimited avatars for your website</p>
+        </div>
 
-          <div className='w-full space-y-4'>
-            {/* Need to update */}
-            <select className='bg-slate-900/60 w-full p-3 rounded-xl'>
-              {data.map((item, index) =>
-                <option key={index} value={item.value}>
-                  {item.value}
-                </option>
-              )}
+        <div className='w-full space-y-4'>
+            <select className='bg-slate-900/60 w-full p-3 rounded-xl' value={option} onChange={onOptionChange}>
+              {
+                data.map((item, index)=>(
+                  <option value={item.value} key={index}>{item.label}</option>
+                ))
+              }
             </select>
-
+            
             <div className='bg-slate-900/60 w-full p-3 rounded-xl'>
-              {/* // link of avatar */}
-              https://raycrops.com
+              {src}
             </div>
-          </div>
+        </div>
 
+        <div className='flex w-full gap-4'>
+          <button onClick={generate} className='flex-1 bg-gradient-to-r from-rose-500 to-orange-600 font-medium rounded-lg py-2 hover:scale-105 trasition-transform'>
+            <i className="ri-arrow-right-up-line mr-1"></i>
+            Change
+          </button>
 
-          <div className='flex w-full gap-4'>
-            <button className='flex-1 bg-gradient-to-r from-rose-500 to-orange-600 font-medium rounded-lg py-2 hover:scale-105 transition-transform'>
-              <i className="ri-arrow-right-up-line"> </i>
-              Change
-            </button>
-            <button className='flex-1 bg-gradient-to-r from-green-500 to-cyan-600 font-medium rounded-lg py-2 hover:scale-105 transition-transform'>
-              <i className="ri-download-line"> </i>
-              Download
-            </button>
-            <button className='flex-1 bg-gradient-to-r from-orange-500 to-amber-600 font-medium rounded-lg py-2 hover:scale-105 transition-transform'>
-              <i className="ri-file-copy-line"> </i>
-              Copy
-            </button>
-          </div>
+          <button onClick={()=>download(src)} className='flex-1 bg-gradient-to-r from-green-500 to-cyan-600 font-medium rounded-lg py-2 hover:scale-105 trasition-transform'>
+            <i className="ri-download-line mr-1"></i>
+            Download
+          </button>
+
+          <button onClick={()=>copy(src)} className='flex-1 bg-gradient-to-r from-orange-500 to-amber-600 font-medium rounded-lg py-2 hover:scale-105 trasition-transform'>
+            <i className="ri-file-copy-line mr-1"></i>
+            Copy
+          </button>
         </div>
       </div>
-
-    </>
+      <ToastContainer />
+    </div>
   )
 }
+
+export default App
